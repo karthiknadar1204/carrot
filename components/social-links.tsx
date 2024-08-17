1330f83197202a430469b59aa33c992d66fd81b6
karthik <karthiknadar1204@gmail.com>
2024-08-17 17:48:35 +0530

    added basic layout for recording and screenshot

---
+++ components/social-links.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const SocialMediaLinks = () => {
+    return (
+        <div className='flex flex-row gap-4'>
+            <a href="https://github.com/karthiknadar1204" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href="https://www.linkedin.com/in/karthik-nadar-b2155a25b/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+        </div>
+    );
+};
+
+export default SocialMediaLinks;
